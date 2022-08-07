c81837a711be0bdf1b2c17992e88d17d927b9506
joebates-gfm <jbates@gofundme.com>
2022-08-07 16:09:48 -0700

    create the inital templtr logic

---
+++ templtr.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+
+base_template = "BaseTemplate"
+
+print("Welcome to Templtr")
+
+dir_to_place_template = input("Where do you want to place the template? ")
+name_of_files_and_folders =input("What would you like to name the template directory and files? ")
+
+# Get the option selected by the user
+def get_users_selected_option(options):
+
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx + 1, element))
+
+    i = input("Please choose a template option: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return int(i) - 1
+    except:
+        pass
+    return None
+
+template_options = ["next_ts"]
+selected_index = get_users_selected_option(template_options)
+selected_template = template_options[selected_index]
+
+# Get the template to copy
+src_dir = f"templates/{selected_template}"
+dst_dir = f"{dir_to_place_template}/{name_of_files_and_folders}/"
+
+# Copy the template files to the user selected directory
+shutil.copytree(src_dir, dst_dir)
+
+# Get file names in the directory
+# Open and rename anything with BaseTemplate
+# Renmae the folders
+list_of_files = os.listdir(dst_dir)
+
+for file in list_of_files:
+    old_name = f"{dst_dir}{file}"
+    new_name = f"{dst_dir}{file.replace(base_template, name_of_files_and_folders)}"
+
+    try:
+        with open(old_name, "r") as f:
+            data = f.read()
+
+            # Searching and replacing the text
+            # using the replace() function
+            data = data.replace(base_template, name_of_files_and_folders)
+            
+        with open(old_name, 'w') as f:
+
+            # Writing the replaced data in our
+            # text file
+            f.write(data)
+    except:
+        print("There was an error updating the file name")
+        
+
+    os.rename(old_name, new_name)
+
+print(selected_index)
