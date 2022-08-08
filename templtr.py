0429a29ff4dfa11e97611c772892ad9ec65c3b81
joebates-gfm <jbates@gofundme.com>
2022-08-07 19:35:51 -0700

    convert to class

--- templtr.py
+++ templtr.py
@@ -4,82 +4,103 @@ import shutil
 base_template = "BaseTemplate"
 templates_dir = "templates"
 
-print("Welcome to Templtr")
 
-dir_to_place_template = input("Where do you want to place the template? ")
-
-dst_dir = None
-name_of_files_and_folders = input("What would you like to name the template directory and files? ")
-
-# Check if the directory exists
-while True:
-    dst_dir = f"{dir_to_place_template}/{name_of_files_and_folders}/"
-
-    if (os.path.exists(dst_dir)):
-        name_of_files_and_folders = input(f"/{name_of_files_and_folders} directory already exists. Please choose a different name: ")
-        continue
-
-    break
-
-# Get the option selected by the user
-def get_users_selected_option(options):
-    for idx, element in enumerate(options):
-        print("{}) {}".format(idx + 1, element))
-
-    i = input("Please choose a template option: ")
-    try:
-        if 0 < int(i) <= len(options):
-            return int(i) - 1
-    except:
+class Templatr():
+    def __init__(self):
         pass
-    return None
 
-def get_template_options(templates_dir):
-    list_of_templates = os.listdir(templates_dir)
-    template_options = []
+    # Get the option selected by the user
+    def get_users_selected_option(self, options):
+        for idx, element in enumerate(options):
+            print("{}) {}".format(idx + 1, element))
 
-    for template in list_of_templates:
-        dir = f"{templates_dir}/{template}"
+        i = input("Please choose a template option: ")
+        try:
+            if 0 < int(i) <= len(options):
+                return int(i) - 1
+        except:
+            pass
+        return None
 
-        if os.path.isdir(dir):
-            template_options.append(template)
+    # Gets the template options
+    def get_template_options(self):
+        list_of_templates = os.listdir(templates_dir)
+        template_options = []
 
-    return template_options
+        for template in list_of_templates:
+            dir = f"{templates_dir}/{template}"
 
-template_options = get_template_options(templates_dir)
-selected_index = get_users_selected_option(template_options)
-selected_template = template_options[selected_index]
+            if os.path.isdir(dir):
+                template_options.append(template)
 
-# Get the template to copy
-src_dir = f"{templates_dir}/{selected_template}"
+        return template_options
 
-# Copy the template files to the user selected directory
-shutil.copytree(src_dir, dst_dir)
+    # Get file names in the directory
+    # Open and rename anything with BaseTemplate
+    # Renmae the folders
+    def rename_copied_files(self, dst_dir, name_of_files_and_folders):
+        list_of_files = os.listdir(dst_dir)
 
-# Get file names in the directory
-# Open and rename anything with BaseTemplate
-# Renmae the folders
-list_of_files = os.listdir(dst_dir)
+        for file in list_of_files:
+            old_name = f"{dst_dir}{file}"
+            new_name = f"{dst_dir}{file.replace(base_template, name_of_files_and_folders)}"
 
-for file in list_of_files:
-    old_name = f"{dst_dir}{file}"
-    new_name = f"{dst_dir}{file.replace(base_template, name_of_files_and_folders)}"
+            try:
+                with open(old_name, "r") as f:
+                    data = f.read()
 
-    try:
-        with open(old_name, "r") as f:
-            data = f.read()
+                    # Searching and replacing the text
+                    # using the replace() function
+                    data = data.replace(
+                        base_template, name_of_files_and_folders)
 
-            # Searching and replacing the text
-            # using the replace() function
-            data = data.replace(base_template, name_of_files_and_folders)
-            
-        with open(old_name, 'w') as f:
+                with open(old_name, 'w') as f:
 
-            # Writing the replaced data in our
-            # text file
-            f.write(data)
-    except:
-        print(f"There was an error updating {file}'s contents")
-        
+                    # Writing the replaced data in our
+                    # text file
+                    f.write(data)
+            except:
+                print(f"There was an error updating {file}'s contents")
 
-    os.rename(old_name, new_name)
+            os.rename(old_name, new_name)
+
+    # Creates a directory and a list of files based off
+    # a users selected template
+    def create_template(self):
+        print("Welcome to Templtr")
+
+        dir_to_place_template = input(
+            "Where do you want to place the template? ")
+
+        dst_dir = None
+        name_of_files_and_folders = input(
+            "What would you like to name the template directory and files? ")
+
+        # Check if the directory exists
+        while True:
+            dst_dir = f"{dir_to_place_template}/{name_of_files_and_folders}/"
+
+            if (os.path.exists(dst_dir)):
+                name_of_files_and_folders = input(
+                    f"{name_of_files_and_folders} directory already exists. Please choose a different name: ")
+                continue
+
+            break
+
+        template_options = self.get_template_options()
+        selected_index = self.get_users_selected_option(template_options)
+        selected_template = template_options[selected_index]
+
+        # Get the template to copy
+        src_dir = f"{templates_dir}/{selected_template}"
+
+        # Copy the template files to the user selected directory
+        shutil.copytree(src_dir, dst_dir)
+
+        # Rename the copied files to match the user created name
+        self.rename_copied_files(dst_dir, name_of_files_and_folders)
+
+
+templatr = Templatr()
+
+templatr.create_template()
