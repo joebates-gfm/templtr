5ae23d55c91f043d2c18730805b29295944f1458
joebates-gfm <jbates@gofundme.com>
2022-08-09 17:05:16 -0700

    initialize an experimental node version

---
+++ templtr.js
@@ -0,0 +1,14 @@
+// Node.js program to demonstrate the   
+// fs.mkdir() Method
+  
+// Include fs and path module
+const fs = require('fs');
+const path = require('path');
+   
+fs.mkdir(path.join(`${__dirname}/test`, 'poop'), (err) => {
+    console.log({err})
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Directory created successfully!');
+});
