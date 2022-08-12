4f354f41d38accd4b06fd095cc0bc28647cbbe23
joebates-gfm <jbates@gofundme.com>
2022-08-12 15:27:41 -0700

    update context base

--- src/templates/react_context/context.tsx
+++ src/templates/react_context/context.tsx
@@ -3,7 +3,17 @@ import React from "react";
 export const BaseTemplate = React.createContext<undefined>(undefined);
 
 export function useBaseTemplate() {
-    const initialState = React.useContext(BaseTemplate);
+    const context = React.useContext(BaseTemplate);
 
-    return initialState || {};
+    if (!context) throw new Error("useBaseTemplate must be used within a BaseTemplateProvider");
+
+    return context;
+}
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export function BaseTemplateProvider({children}: Props) {
+    return <BaseTemplate.Provider value={{}}>{children}</BaseTemplate.Provider>;
 }
