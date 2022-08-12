befb82bf2c9836375de798b7c3a6f27ff0516c81
joebates-gfm <jbates@gofundme.com>
2022-08-12 12:51:16 -0700

    update context boilerplate

--- src/templates/react_context/context.tsx
+++ src/templates/react_context/context.tsx
@@ -1,1 +1,9 @@
 import React from "react";
+
+export const BaseTemplate = React.createContext<undefined>(undefined);
+
+export function useBaseTemplate() {
+    const initialState = React.useContext(BaseTemplate);
+
+    return initialState || {};
+}
