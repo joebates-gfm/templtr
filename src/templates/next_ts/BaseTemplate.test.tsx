720de90de8de30e885293f77fcae2f8f9c57c2f4
jbates <jbates@gofundme.com>
2022-08-30 22:10:47 +0000

    updates to the basetemplate test

--- src/templates/next_ts/BaseTemplate.test.tsx
+++ src/templates/next_ts/BaseTemplate.test.tsx
@@ -2,14 +2,12 @@ import "@testing-library/jest-dom";
 import React from "react";
 import {render} from "@testing-library/react";
 
-import {BaseTemplate} from "./index";
+import BaseTemplate from "./BaseTemplate";
 
 const defaultProps = {};
 
 describe("/BaseTemplate", () => {
     it("should render BaseTemplate component", () => {
-        render(<BaseTemplate {...defaultProps} />);
-
-       
+        render(<BaseTemplate {...defaultProps} />);  
     });
 });
